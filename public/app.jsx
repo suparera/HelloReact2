e5f402f696d01ba642d9f3113fe8a62aaab33064
suparera <suparera@gmail.com>
2017-02-24 10:02:28 +0700

    013

--- public/app.jsx
+++ public/app.jsx
@@ -6,13 +6,37 @@ let Greeter = React.createClass({
     };
   }
 
+  ,getInitialState:function(){
+    return {
+      name: this.props.name
+    }
+  }
+
+  ,onSubmit:function(e){
+    e.preventDefault();
+    let nameRef = this.refs.name;
+    let name = nameRef.value;
+    nameRef.value = '';
+
+    // Validate name, not allow blank
+    if(typeof name === 'string' && name.length>0){
+      this.setState({
+        name:name
+      })
+    }
+  }
+
   // require function for class
   ,render:function(){
-    let name = this.props.name;
+    let name = this.state.name;
     let message = this.props.message;
     return (
       /* can return 1 root div */
       <div>
+        <form onSubmit={this.onSubmit}>
+          <input ref="name" type="text"/>
+          <button>Set Name</button>
+        </form>
         <h1>Hi สวัสดี {name}</h1>
         <p>{message}</p>
       </div>
